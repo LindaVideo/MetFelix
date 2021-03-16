8b926277ba0377300fc5dac7c5fb399398ed6016
Linda Waaijer <linda@Lindas-MBP.2e.lan>
2021-03-16 13:29:13 +0100

    mijn omschrijving

---
+++ metfelix.py
@@ -0,0 +1,14 @@
+getallen = [54, 3, 21, 88, 21]
+
+print("test")
+teller = 0
+
+for getal in getallen:
+		print(getallen[teller])
+		print(getallen[teller+1])
+		if getallen[teller] > getallen[teller+1]:
+			print("ene")
+		else:
+			print("andere")
+		teller = teller + 1
+
